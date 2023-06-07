0b0dc3714736997333fc4c3cef9773411f7b97f5
Ahmed_226 <88344628+ahm226@users.noreply.github.com>
2023-06-07 13:06:02 +0500

    firebase integration

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,90 @@
 import 'package:bordeaux/screens/splash_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:get/get.dart';
 import 'package:sizer/sizer.dart';
 
 import 'controllers/general_controller.dart';
+import 'helpers/constants.dart';
 
-void main() {
-  runApp(const MyApp());
+Future<void> _handleMessage(RemoteMessage message) async {
+  print("_handleMessage");
+  print(message);
+}
+
+Future<void> onSelectNotification(String? payload) async {
+  print("onSelectNotification");
+  print(payload);
+}
+
+Future<void> _selectNotification(RemoteMessage message) async {
+  AndroidNotificationDetails androidPlatformChannelSpecifics =
+      const AndroidNotificationDetails(
+          'high_importance_channel', 'High Importance Notifications',
+          importance: Importance.high,
+          priority: Priority.high,
+          icon: "@mipmap/ic_launcher");
+  NotificationDetails platformChannelSpecifics =
+      NotificationDetails(android: androidPlatformChannelSpecifics);
+  print("message.data");
+  groupNotificationTitle = message.data['groupTitle'];
+
+  await FlutterLocalNotificationsPlugin().show(123, message.notification!.title,
+      message.notification!.body, platformChannelSpecifics,
+      payload: 'data');
+
+  const AndroidInitializationSettings initializationSettingsAndroid =
+      AndroidInitializationSettings('@mipmap/ic_launcher');
+  final IOSInitializationSettings initializationSettingsIOS =
+      IOSInitializationSettings();
+  final InitializationSettings initializationSettings = InitializationSettings(
+      android: initializationSettingsAndroid, iOS: initializationSettingsIOS);
+  FlutterLocalNotificationsPlugin().initialize(initializationSettings,
+      onSelectNotification: onSelectNotification);
+}
+
+Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
+  print('Handling a background message ${message.messageId}');
+}
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
+  FirebaseMessaging.onMessageOpenedApp.listen((_handleMessage));
+  FirebaseMessaging.onMessage.listen((_selectNotification));
+
+  const AndroidNotificationChannel channel = AndroidNotificationChannel(
+      'high_importance_channel', // id
+      'High Importance Notifications', // title // description
+      importance: Importance.high,
+      playSound: true);
+
+  final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
+      FlutterLocalNotificationsPlugin();
+  await flutterLocalNotificationsPlugin
+      .resolvePlatformSpecificImplementation<
+          AndroidFlutterLocalNotificationsPlugin>()
+      ?.createNotificationChannel(channel);
+  await FirebaseMessaging.instance.setForegroundNotificationPresentationOptions(
+    alert: true,
+    badge: true,
+    sound: true,
+  );
+  final NotificationAppLaunchDetails? notificationAppLaunchDetails =
+      await flutterLocalNotificationsPlugin.getNotificationAppLaunchDetails();
+  FirebaseMessaging.instance.requestPermission(
+      sound: true, badge: true, alert: true, provisional: true);
+  /*  payload = await notificationAppLaunchDetails!.payload;
+  if (payload != null) {
+    route = '/second';
+  } */
+//  Stripe.publishableKey =
+  //    "pk_test_51N8u8JAvobntCO1MbPMHxQhCk5liwtZV3yNANTx4ZkFIXBm1LUgFejyLM02EYiSrgzgU1QixFMoJVDPnV6IvVnIG00Fi2iaR7R";
+  runApp(MyApp());
 }
 
 class MyApp extends StatefulWidget {
@@ -21,6 +98,16 @@ class _MyAppState extends State<MyApp> {
   @override
   void initState() {
     super.initState();
+    FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
+    getFCM();
+  }
+
+  getFCM() async {
+    FirebaseMessaging.instance.getToken().then((value) {
+      fcmToken.value = value!;
+      print("Token saved");
+      print(fcmToken.value);
+    });
   }
 
   @override
